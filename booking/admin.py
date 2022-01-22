6c91bdb5816fcf78864fdd018815aff8c375d3bd
J-R77-stack <johnnyramsay81@gmail.com>
2022-01-22 13:52:59 +0000

    com

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Booking
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(ModelAdmin):
+    """
+    Class registered to represent model in admin database.
+    """
+    list_display = ('user', 'name',
+                    'appointment_date_and_time',
+                    'phone_number',
+                    'email')
+    search_fields = ('name',
+                     'appointment_date_and_time',
+                     'phone_number',
+                     'email')
+    list_filter = ('name',
+                   'appointment_date_and_time',
+                   'phone_number',
+                   'email')
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
-# Create your models here.
+class Booking(models.Model):
+    
+    user = models.ForeignKey(User, null=True, blank=True,on_delete=models.CASCADE)
+    name = models.CharField(max_length=80, null=True)
+    appointment_date_and_time = models.DateTimeField(null=True)
+
+    def validate_date(reservation_date_and_time):
+        
+        if reservation_date_and_time < timezone.now():
+            raise ValidationError("Sorry no available appointments on this day. Please try another date and time")
+    appointment_date_and_time = models.DateTimeField(null=True,blank=True,validators=[validate_date])
+    
+    phone_number = models.CharField(null=True, blank=True, max_length=16)
+    email = models.EmailField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        
+        unique_together = ('user', 'name',
+                           'appointment_date_and_time')
+        ordering = ["created_on"]
+
+    def __str__(self):
+        """
+        Function to return object model
+        items as string.
+        """
+        return f' User {self.user} has made an appointment \
+                   for {self.name}\
+                   for {self.appointment_date_and_time}.'
+
+
+
