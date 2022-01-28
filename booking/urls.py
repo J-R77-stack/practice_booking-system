46fef70d3794e3c7c010f9bedf4d54ac1e5b2f82
J-R77-stack <johnnyramsay81@gmail.com>
2022-01-28 17:18:03 +0000

    add styles etc

--- booking/urls.py
+++ booking/urls.py
@@ -4,8 +4,8 @@ from booking import views
 urlpatterns = [
     path('', views.view_home, name='home'),
     path('services/', views.view_services, name='services'),
-    path('contact', views.contact, name='contact'),
-    path('about', views.view_about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.view_about, name='about'),
     path('book/', views.view_book, name='book'),
-    path('blog/', views.view_book, name='blog')
+    path('blog/', views.view_blog, name='blog')
 ]
